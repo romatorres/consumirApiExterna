2d8c338a1b99f944e7e2d9aba67764de364bacdc
romatorres <romatorres12@gmail.com>
2025-03-24 23:17:19 -0300

    atualixar toast

--- frontend/src/Users/App.jsx
+++ frontend/src/Users/App.jsx
@@ -9,15 +9,30 @@ function App() {
     <>
       <Toaster richColors />
       <section className="w-7xl mx-auto mt-10">
-        <div className="flex flex-col justify-center items-center text-center">
+        <div className="flex flex-col items-center justify-center">
+            <div className="flex items-center justify-between max-w-2xl w-full mb-6">
           <h1 className="text-3xl mb-6">Usuarios do Sistema</h1>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded">Novo Usuário</button>
+          </div>
           {users.length > 0 ? (
-            <div>
+            <div className="max-w-2xl w-full">
               {users.map((user) => (
-                <Card key={users.id}>
-                  <div>
+                <Card key={users.id} className="mb-4 px-8">
+                  <div className="flex justify-between items-center">
+                    <div className="flex flex-col gap-1 text-xl">
+                    <div className="flex gap-2 items-start">
+                    <strong>Nome:</strong>
                     <p>{user.name}</p>
+                      </div>
+                      <div className="flex gap-2">
+                        <strong>Email:</strong>
                     <p>{user.email}</p>
+                    </div>
+                    </div>
+                    <div className="flex gap-2">
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded">Editar</button>
+                    <button className="bg-red-500 hover:bg-red-700 text-white font-semibold py-2 px-4 rounded">Excluir</button>
+                    </div>
                   </div>
                 </Card>
               ))}
